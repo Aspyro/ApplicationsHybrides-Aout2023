bd559ebdb0b91eb283ef3c2a0978184336f1b7bc
AspyroDev <aspyrodev@gmail.com>
2023-08-03 20:50:18 +0200

    Récupération des informations du formulaire

--- app/src/main/java/dev/aspyro/androidapplication/AdminFormActivity.kt
+++ app/src/main/java/dev/aspyro/androidapplication/AdminFormActivity.kt
@@ -5,18 +5,31 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 
 class AdminFormActivity : Activity() {
+
+    lateinit var email_edit : EditText
+    lateinit var password_edit : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_form)
+
+        email_edit = findViewById(R.id.et_email_inscriptionAdmin)
+        password_edit = findViewById(R.id.et_password_inscriptionAdmin)
     }
 
     fun onAdminFormClickManager(v : View) {
-        /*when (v.id) {
-            // Toast.makeText(applicationContext,"Direction activité enfant", Toast.LENGTH_LONG).show()
-        }*/
+        when (v.id) {
+            R.id.btn_adminFormValidation -> {
+                Toast.makeText(applicationContext,
+                    "Email : " + email_edit.text + "\nMot de passe : " + password_edit.text,
+                    Toast.LENGTH_LONG)
+                .show()
+            }
+            //Toast.makeText(applicationContext,"Direction activité enfant", Toast.LENGTH_LONG).show()
+        }
 
     }
 
