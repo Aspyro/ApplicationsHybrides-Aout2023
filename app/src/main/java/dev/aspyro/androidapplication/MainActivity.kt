3d794d605c762d18f2a17a134288579868ec5e68
AspyroDev <aspyrodev@gmail.com>
2023-08-03 18:12:36 +0200

    Ajout des fonctions de cycle de vie de l'activité MainActivity

--- app/src/main/java/dev/aspyro/androidapplication/MainActivity.kt
+++ app/src/main/java/dev/aspyro/androidapplication/MainActivity.kt
@@ -2,10 +2,38 @@ package dev.aspyro.androidapplication
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MainActivity", "Méthode OnStart")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("MainActivity", "Méthode onRestart")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i("MainActivity", "Méthode onResume")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.i("MainActivity", "Méthode onPause")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("MainActivity", "Méthode OnDestroy")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("MainActivity", "Méthode OnStop")
     }
 }
