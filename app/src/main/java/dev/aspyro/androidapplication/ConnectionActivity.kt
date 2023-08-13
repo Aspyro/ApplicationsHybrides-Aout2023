581f010e411de4f189581db6231b6a1ef98c1bfa
AspyroDev <aspyrodev@gmail.com>
2023-08-13 17:30:59 +0200

    Création d'une activité pour le formulaire de connexion

---
+++ app/src/main/java/dev/aspyro/androidapplication/ConnectionActivity.kt
@@ -0,0 +1,11 @@
+package dev.aspyro.androidapplication
+
+import android.app.Activity
+import android.os.Bundle
+
+class ConnectionActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_connection)
+    }
+}
