7d0a8a3a4f5d97835e11f5667a2b8b00a3f682c3
AspyroDev <aspyrodev@gmail.com>
2023-08-13 20:28:51 +0200

    Nettoyage de la logique pour l'activité de création d'un utilisateur

--- app/src/main/java/dev/aspyro/androidapplication/UserFormActivity.kt
+++ app/src/main/java/dev/aspyro/androidapplication/UserFormActivity.kt
@@ -4,39 +4,29 @@ import android.app.Activity
 import android.content.Intent
 import android.os.AsyncTask
 import android.os.Bundle
-import android.preference.PreferenceManager
 import android.util.Log
 import android.view.View
 import android.widget.EditText
-import android.widget.Toast
 import androidx.room.Room
 import dev.aspyro.androidapplication.databaseroom.AppDatabase
-import dev.aspyro.androidapplication.databaseroom.User
 import dev.aspyro.androidapplication.databaseroom.UserRecord
 
 class UserFormActivity : Activity() {
 
-    lateinit var email_edit : EditText
-    lateinit var password_edit : EditText
+    private lateinit var emailEdit : EditText
+    private lateinit var passwordEdit : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_form)
 
-        email_edit = findViewById(R.id.et_email_inscription)
-        password_edit = findViewById(R.id.et_password_inscription)
+        emailEdit = findViewById(R.id.et_email_inscription)
+        passwordEdit = findViewById(R.id.et_password_inscription)
     }
 
     fun onFormClickManager(v : View) {
         when (v.id) {
             R.id.btn_FormValidation -> {
-
-                val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(applicationContext)
-                val editor = sharedPreferences.edit()
-
-                editor.putString("email_user", email_edit.text.toString())
-                editor.apply()
-
                 writeUser()
 
                 intent = Intent(this, MainActivity::class.java)
@@ -47,52 +37,55 @@ class UserFormActivity : Activity() {
 
     private fun writeUser() {
 
-        val u = User(0, email_edit.text.toString(), password_edit.text.toString(), 1)
-        Log.i("Database attempt", "Tried to create a user : ${u.toString()}")
-        Toast.makeText(this, u.toString(), Toast.LENGTH_LONG).show()
+        // Récupération des informations
+        val userEmail = emailEdit.text.toString()
+        val userPassword = emailEdit.text.toString()
+        val userAccess = 1
 
+        Log.i("User Creation Form", "Triying to create a User :\n$userEmail - $userPassword")
+
+        // Création de l'utilisateur dans la base de données
         AsyncTask.execute {
             val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "MyDatabase.db")
                 .build()
 
             val dao = db.userDao()
             try {
-                val registeredUser = UserRecord(u.id, u.email, u.pwd, u.access)
+                val registeredUser = UserRecord(0, userEmail, userPassword, userAccess)
                 dao.insertUser(registeredUser)
-                Log.i("test", "try catch dans async")
+                Log.i("User Creation Form", "User created")
             }
             catch (e: Exception) {
-                Log.i("ERROR", e.message.toString())
+                Log.i("User Creation Form", e.message.toString())
             }
 
         }
     }
 
-
     override fun onStart() {
         super.onStart()
-        Log.i("FormActivity", "Méthode OnStart")
+        Log.i("User Creation Form", "Méthode OnStart")
     }
     override fun onRestart() {
         super.onRestart()
-        Log.i("FormActivity", "Méthode onRestart")
+        Log.i("User Creation Form", "Méthode onRestart")
     }
     override fun onResume() {
         super.onResume()
-        Log.i("FormActivity", "Méthode onResume")
+        Log.i("User Creation Form", "Méthode onResume")
     }
     override fun onPause() {
         super.onPause()
-        Log.i("FormActivity", "Méthode onPause")
+        Log.i("User Creation Form", "Méthode onPause")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.i("FormActivity", "Méthode OnDestroy")
+        Log.i("User Creation Form", "Méthode OnDestroy")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.i("FormActivity", "Méthode OnStop")
+        Log.i("User Creation Form", "Méthode OnStop")
     }
 }
