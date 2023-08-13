62be78ff8a0fb564a59b0d34774eaad90fd3fd83
AspyroDev <aspyrodev@gmail.com>
2023-08-13 18:36:15 +0200

    Ajout de valeurs par défaut pour l'entité UserRecord

--- app/src/main/java/dev/aspyro/androidapplication/databaseroom/UserRecord.kt
+++ app/src/main/java/dev/aspyro/androidapplication/databaseroom/UserRecord.kt
@@ -5,7 +5,7 @@ import androidx.room.*
 @Entity(tableName = "UserTable")
 data class UserRecord(
     @ColumnInfo(name = "id") @PrimaryKey(autoGenerate = true) var id: Int = 0,
-    @ColumnInfo(name = "email") var email: String,
-    @ColumnInfo(name = "pwd") var pwd: String,
-    @ColumnInfo(name = "access") var access: Int,
+    @ColumnInfo(name = "email") var email: String = "",
+    @ColumnInfo(name = "pwd") var pwd: String = "",
+    @ColumnInfo(name = "access") var access: Int = 0,
 )
