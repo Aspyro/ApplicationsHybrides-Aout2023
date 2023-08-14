f9d6e5e8f20714adde8f7cd9db27d80b4f92d12b
AspyroDev <aspyrodev@gmail.com>
2023-08-14 13:49:08 +0200

    Vérification des formats pour l'adresse mail et le mot de passe

--- app/src/main/java/dev/aspyro/androidapplication/UserFormActivity.kt
+++ app/src/main/java/dev/aspyro/androidapplication/UserFormActivity.kt
@@ -4,7 +4,9 @@ import android.app.Activity
 import android.content.Intent
 import android.os.AsyncTask
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
+import android.util.Patterns
 import android.view.View
 import android.widget.EditText
 import androidx.room.Room
@@ -27,39 +29,57 @@ class UserFormActivity : Activity() {
     fun onFormClickManager(v : View) {
         when (v.id) {
             R.id.btn_FormValidation -> {
-                writeUser()
-
-                intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
+                if(writeUser()) {
+                    intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
             }
         }
     }
 
-    private fun writeUser() {
+    private fun writeUser() : Boolean {
 
         // Récupération des informations
         val userEmail = emailEdit.text.toString()
-        val userPassword = emailEdit.text.toString()
+        val userPassword = passwordEdit.text.toString()
         val userAccess = 1
 
-        Log.i("User Creation Form", "Triying to create a User :\n$userEmail - $userPassword")
-
-        // Création de l'utilisateur dans la base de données
-        AsyncTask.execute {
-            val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "MyDatabase.db")
-                .build()
-
-            val dao = db.userDao()
-            try {
-                val registeredUser = UserRecord(0, userEmail, userPassword, userAccess)
-                dao.insertUser(registeredUser)
-                Log.i("User Creation Form", "User created")
-            }
-            catch (e: Exception) {
-                Log.i("User Creation Form", e.message.toString())
-            }
-
+        if(!isEmail(userEmail)) {
+            emailEdit.error = "Entrez une adresse email valide."
+            return false
         }
+        else if (!isPasswordLongEnough(userPassword)) {
+            passwordEdit.error = "Votre mot de passe doit faire au moins 4 caractères."
+            return false
+        }
+        else {
+            Log.i("User Creation Form", "Triying to create a User :\n$userEmail - $userPassword")
+
+            // Création de l'utilisateur dans la base de données
+            AsyncTask.execute {
+                val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "MyDatabase.db")
+                    .build()
+
+                val dao = db.userDao()
+                try {
+                    val registeredUser = UserRecord(0, userEmail, userPassword, userAccess)
+                    dao.insertUser(registeredUser)
+                    Log.i("User Creation Form", "User created")
+                }
+                catch (e: Exception) {
+                    Log.i("User Creation Form", e.message.toString())
+                }
+            }
+        }
+        return true
+    }
+
+    private fun isPasswordLongEnough(userPassword: String): Boolean {
+        return userPassword.length >= 4
+    }
+
+    private fun isEmail(userEmail: String): Boolean {
+        return (!TextUtils.isEmpty(userEmail) && Patterns.EMAIL_ADDRESS.matcher(userEmail).matches())
     }
 
     override fun onStart() {
