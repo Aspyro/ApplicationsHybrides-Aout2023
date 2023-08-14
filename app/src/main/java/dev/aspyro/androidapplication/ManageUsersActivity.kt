85257123598ff85d5cc4ab98d7374930cc753fec
AspyroDev <aspyrodev@gmail.com>
2023-08-14 16:21:38 +0200

    Création de l'activité ManageAssets et de l'activité ManageUsers

---
+++ app/src/main/java/dev/aspyro/androidapplication/ManageUsersActivity.kt
@@ -0,0 +1,11 @@
+package dev.aspyro.androidapplication
+
+import android.app.Activity
+import android.os.Bundle
+
+class ManageUsersActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manage_users)
+    }
+}
