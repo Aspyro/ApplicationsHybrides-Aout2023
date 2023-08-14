57e463e1b71dd5ee7ce55397c3cc0dfc49e807e1
AspyroDev <aspyrodev@gmail.com>
2023-08-14 14:19:19 +0200

    Création de la classe AssetRecord pour la base de données

---
+++ app/src/main/java/dev/aspyro/androidapplication/databaseroom/AssetRecord.kt
@@ -0,0 +1,13 @@
+package dev.aspyro.androidapplication.databaseroom
+
+import androidx.room.*
+
+@Entity(tableName="AssetTable")
+data class AssetRecord(
+    @ColumnInfo(name = "id") @PrimaryKey(autoGenerate = true) var id: Int = 0,
+    @ColumnInfo(name = "hardware") var hardware: String = "",
+    @ColumnInfo(name = "brand") var brand: String = "",
+    @ColumnInfo(name = "model") var model: String = "",
+    @ColumnInfo(name = "reference") var reference: String = "",
+    @ColumnInfo(name = "status") var status: String = "",
+)
